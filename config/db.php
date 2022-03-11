75badc900f426c54a27158dae0694bfc25b3a251
Alex Leoniuk <alex.brest.by@gmail.com>
2022-03-11 12:39:15 +0300

    feat: add user identification

--- config/db.php
+++ config/db.php
@@ -1,2 +1,3 @@
 <?php
 $conn = mysqli_connect("localhost", "root", "453644", "pte_test");
+mysqli_set_charset($conn, "utf8");
--- log.php
+++ log.php
@@ -1,50 +1,103 @@
 <?php
-function getIp() {
+require_once 'config/db.php';
+// ini_set('display_startup_errors', 1);
+// ini_set('display_errors', 1);
+// error_reporting(-1);
+
+$name = "Alex";
+
+if (session_status() === PHP_SESSION_NONE) {
+    $lifetime = 60 * 60 * 24 * 30;
+    // setcookie(session_name(), session_id(), time() + $lifetime);
+
+    session_start(["cookie_lifetime" => '0']);
+
+    if (!isset($_COOKIE["id"])) {
+        $newUserUniqueId = uniqid($prefix = "pte-");
+        setcookie("id", $newUserUniqueId, time() + $lifetime);
+        // Check connection
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+
+        $sql = "INSERT INTO users (unique_id, name, last_visit_at)
+VALUES ('$newUserUniqueId', '$name', CURRENT_TIMESTAMP)";
+
+        if (mysqli_query($conn, $sql)) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+
+        mysqli_close($conn);
+    }
+
+}
+
+//var_dump($_COOKIE["visited"]); //default NULL
+
+// debug(session_id()); //default NULL
+
+// var_dump(session_status());
+
+// var_dump(session_id());
+
+// debug($_SESSION);
+
+// debug(session_get_cookie_params());
+
+$token = "1995157538:AAGFeHeDBkuPC-ABz0qek7xqkcnua8mbwGM";
+$chatid = "317794726";
+
+function getIp()
+{
     $keys = [
-      'HTTP_CLIENT_IP',
-      'HTTP_X_FORWARDED_FOR',
-      'REMOTE_ADDR',
+        'HTTP_CLIENT_IP',
+        'HTTP_X_FORWARDED_FOR',
+        'REMOTE_ADDR',
     ];
 
     foreach ($keys as $key) {
-      if(!empty($_SERVER[$key])) {
-        $ip = trim(end(explode(',', $_SERVER[$key])));
-        if(filter_var($ip, FILTER_VALIDATE_IP)) {
-          return $ip;
+        if (!empty($_SERVER[$key])) {
+            $ip = trim(end(explode(',', $_SERVER[$key])));
+            if (filter_var($ip, FILTER_VALIDATE_IP)) {
+                return $ip;
+            }
         }
-      }
     }
-  }
+}
 
-  $ip = getIp();
-  $info = file_get_contents('http://ip-api.com/json/' . $ip . '?lang=ru');
-  $message = json_decode($info, TRUE);
-
-  $file = 'log.txt';
-  $data = "IP - " . $message["query"] . 
-          "\r\nстрана - " .$message["country"] . 
-          "\r\nгород - " . $message["city"] . 
-          "\r\nпровайдер - "  . $message["isp"] .
-          "\r\nвход выполнен: " . date("Y-m-d H:i:s") .
-          "\r\n\r\n";
-
-  // array(14) { ["status"]=> string(7) "success" ["country"]=> string(12) "Канада" ["countryCode"]=> string(2) "CA" ["region"]=> string(2) "QC" ["regionName"]=> string(6) "Quebec" ["city"]=> string(16) "Монреаль" ["zip"]=> string(3) "H3A" ["lat"]=> float(45.5063) ["lon"]=> float(-73.5794) ["timezone"]=> string(15) "America/Toronto" ["isp"]=> string(7) "OVH SAS" ["org"]=> string(17) "OVH Hosting, Inc." ["as"]=> string(15) "AS16276 OVH SAS" ["query"]=> string(15) "167.114.203.199" }
-
-  // file_put_contents($file, $data, FILE_APPEND);
-
-
-  $token = "1995157538:AAGFeHeDBkuPC-ABz0qek7xqkcnua8mbwGM";
-  $chatid = "317794726";
-
-  function sendMessage($chatID, $messaggio, $token) {
+function sendMessageToTelegram($chatID, $messaggio, $token)
+{
 
     $url = "https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $chatID;
     $url = $url . "&text=" . urlencode($messaggio);
 
-
     $result = file_get_contents($url);
-    // var_dump($result);
     return $result;
 }
 
-// sendMessage($chatid, "На сайт выполнен вход!\r\n" . $data, $token);
+$ip = getIp();
+$info = file_get_contents('http://ip-api.com/json/' . $ip . '?lang=ru');
+$message = json_decode($info, true);
+
+$file = 'log.txt';
+$counterVisit = $_COOKIE["visited"] > 0 ? $_COOKIE["visited"] . " посещение" : "первый раз";
+$data = "IP - " . $message["query"] .
+"\r\nстрана - " . $message["country"] .
+"\r\nгород - " . $message["city"] .
+"\r\nпровайдер - " . $message["isp"] .
+"\r\n" . $counterVisit .
+"\r\nвход выполнен: " . date("Y-m-d H:i:s") .
+    "\r\n\r\n";
+
+// file_put_contents($file, $data, FILE_APPEND);
+
+// sendMessageToTelegram($chatid, "На сайт выполнен вход!\r\n" . $data, $token);
+
+function debug($param)
+{
+    echo "<pre>";
+    var_dump($param);
+    echo "</pre>";
+}
